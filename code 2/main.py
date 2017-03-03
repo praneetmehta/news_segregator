980a4871c1a17825dc7508527f76b1a420ab2e3c
praneetmehta <praneet.mehta@gmail.com>
2017-03-03 19:11:34 +0530

    accuracy calc added(hardcoded)

--- code 2/main.py
+++ code 2/main.py
@@ -4,7 +4,7 @@ Created on Fri Mar  3 03:45:45 2017
 
 @author: praneet
 """
-
+from __future__ import division, print_function
 from sklearn.feature_extraction.text import  TfidfVectorizer
 import reader as rd
 import re
@@ -12,46 +12,61 @@ from sklearn.cluster import KMeans
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
-   
+import lemmatizer as lm
+from tqdm import tqdm
+
+#%%
+def correct(s):
+        s = re.sub('\s\W',' ',s)
+        s = re.sub('\W\s',' ',s)
+        s = re.sub("[^a-zA-Z']",' ', s)
+        s = re.sub('\s+',' ',s)
+        return s
 #meandistortions.append(sum(np.min(cdist(text_vector, kmeans.cluster_centers_, 'euclidean'), axis=1))/text_vector.shape[0])
 #%%
-def plot(kmeans, vectorizer, name):
+def plot(name):
     text_test = (rd.filereader([name], '../train/'))
-    text_test = pd.Series([re.sub("[^a-zA-Z]"," ",test) for test in text_test])
+    text_test = pd.Series([correct(test) for test in text_test])
     x = range(0,text_test.shape[0])
     y = [kmeans.predict(a) for a in vectorizer.transform(text_test)]
     plt.scatter(x,y)
 
 #%%
-def assign(categories, parent, kmeans, vectorizer):
+def assign():   
     assignment = []
     possible = []
-    for i in categories:
-        test_text = rd.filereader([i],parent)
-        test_text = pd.Series([re.sub("[^a-zA-Z]"," ",test) for test in test_text])
+    true_count = 0
+    for i in tqdm(categories):        
+        test_text = rd.filereader([i],'../train/')
+        test_text = pd.Series([correct(test) for test in test_text])
         val = [kmeans.predict(a)[0] for a in vectorizer.transform(test_text)]
         count = Counter(val)
-        print count
+        print(count)
         if count.most_common(1)[0][0] in possible:
             assignment.append((count.most_common(2)[1][0],i))
+            true_count += count.most_common(2)[1][1]
         else:
-            assignment.append((count.most_common(1)[0][0],i))            
-        possible.append(count.most_common(1)[0][0])
-    return dict(assignment)
+            assignment.append((count.most_common(1)[0][0],i))  
+            true_count += count.most_common(1)[0][1]
+        possible.append(count.most_common(1)[0][1])
+    accuracy = float(((true_count)/shape)*100)
+    return dict(assignment), accuracy, true_count
      
 #%%
-def predict(assignment, kmeans, vectorizer):
+def predict():
         text = open('./test.txt','r').read()
-        return assignment[kmeans.predict(vectorizer.transform([re.sub("[^a-zA-Z]"," ",text)]))[0]] 
+        return assignment[kmeans.predict(vectorizer.transform([correct(text)]))[0]] 
          
 #%%
 categories = ['business','entertainment','politics','sport','tech']
 text = pd.Series(rd.filereader(categories, '../train/'))
-text = [re.sub('[^a-zA-Z]',' ',t) for t in text]
+shape = text.shape[0]
+text = [correct(t) for t in text]
+text = lm.wordtokenize(text)
 vectorizer = TfidfVectorizer(stop_words='english', encoding='iso-8859-1', ngram_range=(1,2))
 text_vector = vectorizer.fit_transform(text)
 kmeans = KMeans(n_clusters=5)
 kmeans.fit(text_vector)
 assignment = []
-assignment = assign(categories, '../train/', kmeans, vectorizer)
-print assignment
+assignment, error = assign()
+print (assignment)
